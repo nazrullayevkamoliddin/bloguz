a03ae14f952f922d13a6018fef13e8d347797492
nazrullayevkamoliddin <kamolxoja.n@gmail.com>
2023-12-22 17:57:32 +0500

    Routers Set Up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
+import {Routes, Route} from 'react-router-dom';
 import Navbar from "./components/navbar.component";
 
 const App = () => {
     return (
-        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Navbar/>}>
+                <Route path='signin' element={<h1>Sign In</h1>}/>
+                <Route path='signup' element={<h1>Sign Up</h1>}/>
+            </Route>
+        </Routes>
     )
 }
 
